a16bbf3b41fe375f387a38fdab56d6dd235cd8e8
Arun <arun13e@gmail.com>
2018-05-26 02:48:21 +0530

    .

--- ROW_BUFFER/ROW_BUFFER.vhd
+++ ROW_BUFFER/ROW_BUFFER.vhd
@@ -16,7 +16,7 @@ use IEEE.std_logic_unsigned.all;
 entity ROW_BUFFER is
     port (
         clk, wr, load, bit_md : in std_logic;                                              --bit_md: update shift register with new data
-		rd,ready : out std_logic;                                                          --ready indicator for synthesizer
+        rd,ready : out std_logic;                                                          --ready indicator for synthesizer
         data     : in std_logic_vector(11 downto 0);
         shift    : in std_logic_vector(127 downto 0);	                                   --shift control lines
         rm_addr  : out std_logic_vector(12 downto 0);                                      --address for ram
@@ -43,10 +43,10 @@ begin
         if rising_edge(clk) then
 
             if load = '1' then                                                              --sets ld_flag
-				ready   <= '0';
+                ready   <= '0';
                 ld_flag := '1';
                 addr    := (others => '0');
-				last_wr_flag := '0';
+				        last_wr_flag := '0';
             end if;
 
             if ld_flag = '1' then                                                           --stays in this control until buffer filled
@@ -62,28 +62,28 @@ begin
 
                 elsif last_wr_flag = '1' then
 
-				    for I in 0 to 127 loop						                            --updating shift register after last write
+				            for I in 0 to 127 loop						                                     --updating shift register after last write
                         shift_reg(I) := row_var(I, bit_ch);
                     end loop;
 
-					ready   <= '1';
-					ld_flag := '0';
+				          	ready   <= '1';
+				          	ld_flag := '0';
 
                 else
                     rd <= '1';
                     rm_addr <= addr;
                     wr_flag := '1';
 
-					if addr = x"1FFF" then
-					    last_wr_flag := '1';
-					end if;
+					          if addr = x"1FFF" then
+					              last_wr_flag := '1';
+					          end if;
 
                 end if;
 
             else
 
                 if bit_md = '1' then
-				    bit_ch := bit_ch + 1;
+				            bit_ch := bit_ch + 1;
 
                     for I in 0 to 127 loop
                         shift_reg(I) := row_var(I, bit_ch);
@@ -101,7 +101,7 @@ begin
                 end loop;
 
             end if;
-		end if;
+		    end if;
 
     end process;
 
