43381d8e9fb22dce4dcf428fc696727640606d01
Arun <arun13e@gmail.com>
2018-05-27 11:52:35 +0530

    .

--- SPI/SPI.vhd
+++ SPI/SPI.vhd
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------
---  ROW_BUFFER.vhd
+--  SPI.vhd
 --
 --  This program is free software: you can redistribute it and/or
 --  modify it under the terms of the GNU General Public License
